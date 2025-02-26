b46425ec5770747d6921ecd2e715756e6a701f3f
Kbyungs <kbsoo0620@icloud.com>
2025-02-26 17:34:52 +0900

    배포 준비 - cors

--- backend/src/main/java/homerun2/backend/config/SecurityConfig.java
+++ backend/src/main/java/homerun2/backend/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(frontendUrl));
+        configuration.setAllowedOrigins(Arrays.asList(frontendUrl, "https://homerun2.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
